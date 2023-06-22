fdc669379fcac8131de3538e4b5819879350cd6a
Chhavi <chhavisharma3007@gmail.com>
2023-06-22 21:12:33 +0530

    Implemented second recycler view

--- app/src/main/java/com/example/matchpeoples/MainActivity.kt
+++ app/src/main/java/com/example/matchpeoples/MainActivity.kt
@@ -4,17 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
     private lateinit var matchAdapter: MatchAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        init()
+        init(recyclerView)
+        init(recyclerView2)
         val profileList = listOf(
             UserModel("Emily", 25, "Painter", R.drawable.carsouel1),
             UserModel("John", 30, "Doctor", R.drawable.carousel2),
@@ -23,11 +24,11 @@ class MainActivity : AppCompatActivity() {
         )
         matchAdapter = MatchAdapter(profileList, this)
         recyclerView.adapter = matchAdapter
+        recyclerView2.adapter=matchAdapter
     }
 
 
-    private fun init(){
-        recyclerView = findViewById(R.id.recyclerView)
+    private fun init(recyclerView: RecyclerView){
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(this,RecyclerView.HORIZONTAL,false)
     }
