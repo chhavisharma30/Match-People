6752d0cc1a65cd017b460c1e4ab1aa56cce6bc1a
Chhavi <chhavisharma3007@gmail.com>
2023-06-28 21:45:12 +0530

    Designed basic layout of See All activity

---
+++ app/src/main/java/com/example/matchpeoples/SeeAll.kt
@@ -0,0 +1,11 @@
+package com.example.matchpeoples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SeeAll : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_see_all)
+    }
+}
