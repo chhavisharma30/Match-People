4fa0fc121a9240129fad5add9530a3db1b20db08
Chhavi <chhavisharma3007@gmail.com>
2023-07-06 22:40:33 +0530

    Designed basic layout of Matched activity

---
+++ app/src/main/java/com/example/matchpeoples/Matched.kt
@@ -0,0 +1,11 @@
+package com.example.matchpeoples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Matched : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_matched)
+    }
+}
