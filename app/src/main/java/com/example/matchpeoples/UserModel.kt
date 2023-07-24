5eb85a1e4e9391f74fb6a498087a2d4cfd80ccf7
Rishabh Kumar <rishabh845401@gmail.com>
2023-07-24 22:45:20 +0530

    Created Ui of Premium Plan Activity

--- app/src/main/java/com/example/matchpeoples/UserModel.kt
+++ app/src/main/java/com/example/matchpeoples/UserModel.kt
@@ -4,7 +4,7 @@ data class UserModel(
     val name: String,
     val age: Int,
     val occupation: String,
-    val ImageUrl: String //change this to URL when implementing
+    val ImageUrl: String 
     //val about: String,
     //val interests : ArrayList<String>
 
