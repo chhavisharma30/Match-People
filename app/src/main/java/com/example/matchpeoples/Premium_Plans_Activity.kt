76a6fd110400b0f9e9a4b6de57d5f14de9e6e322
Rishabh Kumar <rishabh845401@gmail.com>
2023-07-24 23:23:04 +0530

    Updated Code

---
+++ app/src/main/java/com/example/matchpeoples/Premium_Plans_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.matchpeoples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Premium_Plans_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_premium_plans2)
+    }
+}
