25e869120baf2c1ee85bf552d1d0cbef7b20483e
Rishabh Kumar <rishabh845401@gmail.com>
2023-07-26 23:32:04 +0530

    Created user interface of gold premium

--- app/src/main/java/com/example/matchpeoples/Premium_Plans_Activity.kt
+++ app/src/main/java/com/example/matchpeoples/Premium_Plans_Activity.kt
@@ -2,10 +2,18 @@ package com.example.matchpeoples
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 
 class Premium_Plans_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_premium_plans2)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.activity_premium_plans)
+        //hiding action bar
+        supportActionBar?.hide()
+        //making status bar transparent
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
     }
 }
